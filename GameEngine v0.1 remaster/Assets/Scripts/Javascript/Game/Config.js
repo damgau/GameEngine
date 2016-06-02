05318df910849e4ab0e25d55baf3f3fae9c4dc28
damgau <damien.gauthier3003@gmail.com>
2016-06-02 10:22:17 +0200

    remaster GameEngine v1

---
+++ GameEngine v0.1 remaster/Assets/Scripts/Javascript/Game/Config.js
@@ -0,0 +1,37 @@
+/**
+ * Get Canvas and the context<br/>
+ *
+ * - Create Scene Object which will contain the scene<br/>
+ * - Create and set the gravity<br/>
+ * - Application : An Object which will handle the scene to load, if game is paused or not and if debug mode is activate<br/>
+ * - imagesLoaded : counter for loaded images<br/>
+ * - WalkableTiles : an Array which will contain where integer where we can walk<br/>
+ * - ImagesPath : Array of image object. Each image has a name and a path<br/>
+ * - Images : an object which contain all loaded image
+ * 
+ * */
+
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+
+var Scenes = {};
+var Gravity = new Vector();
+Gravity.y = -9.81;
+
+
+var Application = 
+{
+	LoadedScene: null,
+	gamePaused: false,
+	debugMode: false
+};
+
+var imagesLoaded = 0;
+var WalkableTiles = [];
+
+var ImagesPath = 
+[
+	// { name:"monImage",path: "background/image.png"},
+];
+var Images = {};
